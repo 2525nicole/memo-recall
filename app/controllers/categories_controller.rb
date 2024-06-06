794d7efff6399f9d923264e9843b72ced074cfea
nicole2525 <nicole2525nomail@gmail.com>
2024-06-06 21:15:58 +0900

    categoriesコントローラー、ビューの生成とroutesの設定をした

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+class CategoriesController < ApplicationController
+  before_action :set_category, only: %i[show edit update destroy]
+
+  def index
+    @categories = Category.all
+  end
+
+  def show; end
+
+  def new
+    @category = Category.new
+  end
+
+  def edit; end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to @category, notice: 'Category was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @category.update(category_params)
+      redirect_to @category, notice: 'Category was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @category.destroy
+    redirect_to categories_url, notice: 'Category was successfully destroyed.'
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:name, :user_id)
+  end
+end
