6efa31357f5484b8b5dcc4bb3c78a9cae27fe2a9
nicole2525 <nicole2525nomail@gmail.com>
2024-06-20 12:56:11 +0900

    メール認証機能を追加した

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@
 
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
 
   has_many :memories, dependent: :destroy
   has_many :categories, dependent: :destroy
