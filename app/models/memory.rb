04ec0a0197c5ff5d87c64952a006b352382a00c4
nicole2525 <nicole2525nomail@gmail.com>
2024-07-02 21:02:12 +0900

    rubocop/linterの指摘事項を修正した

--- app/models/memory.rb
+++ app/models/memory.rb
@@ -8,7 +8,7 @@ class Memory < ApplicationRecord
 
   paginates_per 10
 
-  def self.ransackable_attributes(auth_object = nil)
+  def self.ransackable_attributes(_auth_object = nil)
     ['id']
   end
 end
