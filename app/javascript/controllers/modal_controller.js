466ae71b6606942a5507887a33e3309ab2293eb2
nicole2525 <nicole2525nomail@gmail.com>
2024-07-07 13:24:08 +0900

    思い出一覧ページをTurbo Frames化、思い出の登録と編集フォームをモーダル化した

---
+++ app/javascript/controllers/modal_controller.js
@@ -0,0 +1,13 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  static targets = ["modal"];
+
+  open() {
+    this.modalTarget.classList.remove("hidden");
+  }
+
+  close() {
+    this.modalTarget.classList.add("hidden");
+  }
+}
