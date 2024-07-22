3f9e0211bfbd9feee8d7964fc7e3d2f00378f202
nicole2525 <nicole2525nomail@gmail.com>
2024-07-22 20:14:49 +0900

    思い出登録フォームからカテゴリーも登録できるように変更

--- app/controllers/memories_controller.rb
+++ app/controllers/memories_controller.rb
@@ -2,7 +2,8 @@
 
 class MemoriesController < ApplicationController
   before_action :set_memory, only: %i[edit update destroy]
-  before_action :set_categories, only: %i[new edit]
+  before_action :set_categories, only: %i[new edit create]
+  before_action :initialize_category_errors, only: %i[new create edit update]
 
   def random
     @memory = current_user.memories.order('RANDOM()').first
@@ -22,12 +23,16 @@ class MemoriesController < ApplicationController
 
   def create
     @memory = current_user.memories.build(memory_params)
-    if @memory.save
-      if request.referer.include?(memories_path)
-        flash.now[:after_create] = t('notice.create.memory')
-      else
-        flash.now[:after_create_with_link] = view_context.link_to('思い出を記録しました。（思い出の一覧を見る）', memories_path) unless request.referer.include?(memories_path)
-      end
+
+    if params[:memory][:new_category_name].present?
+      validate_and_set_category
+    elsif params[:memory][:category_id].present?
+      set_existing_category
+    end
+
+    if @memory.valid? && valid_category?
+      @memory.save
+      display_appropriate_flash_messages
     else
       set_categories
       render :new, status: :unprocessable_entity
@@ -58,7 +63,33 @@ class MemoriesController < ApplicationController
     @categories = current_user.categories.all
   end
 
+  def initialize_category_errors
+    @category_errors = []
+  end
+
   def memory_params
     params.require(:memory).permit(:content, :category_id)
+  end
+
+  def validate_and_set_category
+    @category = current_user.categories.find_or_initialize_by(name: params[:memory][:new_category_name])
+    @category.errors.full_messages.each { |msg| @category_errors << msg } unless @category.valid?
+    @memory.category = @category
+  end
+
+  def set_existing_category
+    @memory.category = current_user.categories.find(params[:memory][:category_id])
+  end
+
+  def valid_category?
+    @memory.category.nil? || @memory.category&.valid?
+  end
+
+  def display_appropriate_flash_messages
+    if request.referer.include?(memories_path)
+      flash.now[:after_create] = t('notice.create.memory')
+    else
+      flash.now[:after_create_with_link] = view_context.link_to('思い出を記録しました。（思い出の一覧を見る）', memories_path) unless request.referer.include?(memories_path)
+    end
   end
 end
