db04d8e655ff35edf6e0b20dec4eabc5c0e1dbd6
nicole2525 <nicole2525nomail@gmail.com>
2024-08-27 21:46:52 +0900

    OGPとmetaタグを設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,27 @@ module ApplicationHelper
   def container_padding_class
     user_signed_in? ? 'pt-14' : 'pt-0'
   end
+
+  def default_meta_tags
+    {
+      site: 'Memo Recall',
+      reverse: true,
+      charset: 'utf-8',
+      description: 'Memo Recallは、いつまでも覚えておきたい大事な思い出をいつでも簡単に記録できるサービスです。',
+      keywords: '思い出, 記録',
+      separator: '|',
+      og: {
+        site_name: :site,
+        title: :title,
+        type: 'website',
+        description: :description,
+        image: image_url('ogp.png'),
+        url: 'http://localhost:3000/'
+      },
+      twitter: {
+        card: 'summary_large_image',
+        site: '@2525nicole2525'
+      }
+    }
+  end
 end
