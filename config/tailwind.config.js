d7f207d3d062bcbcc369ee4197d8e601027b72d6
nicole2525 <nicole2525nomail@gmail.com>
2024-10-01 20:58:41 +0900

    カテゴリー一覧ページのレイアウトを修正

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -12,6 +12,9 @@ module.exports = {
       fontFamily: {
         sans: ["Inter var", ...defaultTheme.fontFamily.sans],
       },
+      zIndex: {
+        60: "60",
+      },
     },
   },
   plugins: [
