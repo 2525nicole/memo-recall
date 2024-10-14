2126ecfacf043e95abb76571730fc613c8b6e8a9
nicole2525 <nicole2525nomail@gmail.com>
2024-10-14 17:40:34 +0900

    seedデータを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,14 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# frozen_string_literal: true
+
+user = User.find_or_create_by!(email: 'test@example.com') do |u|
+  u.password = 'password'
+  u.password_confirmation = 'password'
+  u.confirmed_at = Time.now
+end
+
+category = Category.find_or_create_by!(name: '嬉しかったこと', user: user)
+
+# カテゴリーに紐づく思い出
+Memory.find_or_create_by!(content: '今日はこんな嬉しいことがありました！', user: user, category: category)
+# カテゴリーに紐づかない思い出
+Memory.find_or_create_by!(content: 'カテゴリーなしの思い出', user: user, category: nil)
