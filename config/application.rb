b4f8136ca2b60f5c6b5bb440124790a1fba769a4
nicole2525 <nicole2525nomail@gmail.com>
2024-10-29 20:55:09 +0900

    国際化関連の不要な記述を削除

--- config/application.rb
+++ config/application.rb
@@ -49,7 +49,6 @@ module MemoRecall
     end
 
     config.i18n.default_locale = :ja
-    config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}')]
 
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
