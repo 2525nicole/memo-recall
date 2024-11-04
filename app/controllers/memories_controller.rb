81cc3a2d2cb241c956747fc7b775943a64d55556
nicole2525 <nicole2525nomail@gmail.com>
2024-11-04 13:51:23 +0900

    思い出登録時のカテゴリー登録方法を変更

--- app/controllers/memories_controller.rb
+++ app/controllers/memories_controller.rb
@@ -20,47 +20,25 @@ class MemoriesController < ApplicationController
   def edit; end
 
   def create
-    success = false
+    @memory = current_user.memories.new(memory_params)
 
-    ActiveRecord::Base.transaction do
-      @memory = current_user.memories.build(memory_params.except(:new_category_name))
-
-      @category, @category_errors = @memory.assign_category(memory_params, current_user)
-
-      raise ActiveRecord::Rollback unless @memory.valid? && @memory.valid_category?
-
-      @memory.save!
-      set_flash_and_update_page(@memory, @category)
-      success = true
+    if @memory.save
+      set_flash_and_update_page(@memory, @memory.category)
+    else
+      set_categories
+      render :new, status: :unprocessable_entity
     end
-
-    return if success
-
-    set_categories
-    render :new, status: :unprocessable_entity
   end
 
   def update
     old_category_id = @memory.category_id
-    success = false
 
-    ActiveRecord::Base.transaction do
-      @memory.assign_attributes(memory_params.except(:new_category_name))
-
-      @category, @category_errors = @memory.assign_category(memory_params, current_user)
-
-      raise ActiveRecord::Rollback unless @memory.valid? && @memory.valid_category?
-
-      @memory.save!
+    if @memory.update(memory_params)
       handle_update_flash_and_render(old_category_id)
-
-      success = true
+    else
+      set_categories
+      render :edit, status: :unprocessable_entity
     end
-
-    return if success
-
-    set_categories
-    render :edit, status: :unprocessable_entity
   end
 
   def destroy
--- spec/models/memory_spec.rb
+++ spec/models/memory_spec.rb
@@ -12,69 +12,47 @@ RSpec.describe Memory, type: :model do
     end
   end
 
-  describe '#valid_category?' do
-    context 'category が nilの場合' do
-      it 'true を返す' do
-        memory = create(:memory, user: confirm_user, category: nil)
-        expect(memory.valid_category?).to eq(true)
-      end
-    end
-
-    context 'category が有効な場合' do
-      it 'true を返す' do
-        memory = create(:memory, user: confirm_user, category: valid_category)
-        expect(memory.valid_category?).to eq(true)
-      end
-    end
-
-    context 'category 無効な場合' do
-      it 'false を返す' do
-        category = build(:category, name: nil, user: confirm_user)
-        memory = create(:memory, user: confirm_user, category:)
-        expect(memory.valid_category?).to eq(false)
-      end
-    end
-  end
-
   describe '#assign_category' do
     context '新しいカテゴリー名が渡された場合' do
       it 'カテゴリーが作成され、思い出に紐づけられる' do
-        memory = build(:memory, user: confirm_user)
-
-        category, category_errors = memory.assign_category({ new_category_name: '新しいカテゴリー' }, confirm_user)
-
-        expect(category.name).to eq('新しいカテゴリー')
-        expect(memory.category).to eq(category)
-        expect(category_errors).to be_empty
-      end
-
-      it 'カテゴリーが invalid の場合、エラーが返される' do
-        memory = build(:memory, user: confirm_user)
-        _category, category_errors = memory.assign_category({ new_category_name: 'x' * 20 }, confirm_user)
-
-        expect(category_errors).not_to be_empty
-        expect(category_errors).to include('カテゴリー名は15文字以内で入力してください')
+        memory = build(:memory, user: confirm_user, new_category_name: '新しいカテゴリー')
+        memory.save
+        expect(memory.category.name).to eq('新しいカテゴリー')
       end
     end
 
     context '登録済みのカテゴリーIDが渡された場合' do
       it '登録済みのカテゴリーが思い出に紐づけられる' do
-        memory = build(:memory, user: confirm_user)
-        _category, category_errors = memory.assign_category({ category_id: valid_category.id }, confirm_user)
-
+        memory = build(:memory, user: confirm_user, category: valid_category)
+        memory.save
         expect(memory.category).to eq(valid_category)
-        expect(category_errors).to be_empty
       end
     end
 
     context '新しいカテゴリー名も登録済みのカテゴリーIDも渡されない場合' do
       it 'カテゴリーは思い出に紐づけられない' do
-        memory = build(:memory, user: confirm_user)
-        _category, category_errors = memory.assign_category({}, confirm_user)
-
+        memory = build(:memory, user: confirm_user, category: nil)
+        memory.save
         expect(memory.category).to be_nil
-        expect(category_errors).to be_empty
       end
+    end
+  end
+
+  describe '#validate_associated_category' do
+    it 'カテゴリーが invalid の場合、エラーが追加される' do
+      invalid_category = build(:category, name: nil, user: confirm_user)
+      memory = build(:memory, user: confirm_user, category: invalid_category)
+      memory.valid?
+
+      expect(memory.errors[:base]).to include('カテゴリー名を入力してください')
+    end
+
+    it 'カテゴリーが valid の場合、エラーは追加されない' do
+      valid_category = build(:category, user: confirm_user)
+      memory = build(:memory, user: confirm_user, category: valid_category)
+      memory.valid?
+
+      expect(memory.errors[:base]).to be_empty
     end
   end
 end
